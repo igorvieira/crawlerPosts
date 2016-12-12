5e28203c6dbe79265d733265a2df6024a9193070
Igor Vieira <igor.p.r.vieira@gmail.com>
2016-12-12 00:32:04 -0200

    set for consign

--- server/config/config.test.js
+++ server/config/config.test.js
@@ -1,13 +1,19 @@
 export default {
   isTest: true,
   server: {
-    port: 3000,
+    port: 3030,
     host: 'localhost'
   },
   bodyParser: {
     extended: true
   },
   mongodb: {
-    uri: 'mongodb://localhost:27017/lettpost_test'
-  }
+    uri: 'mongodb://localhost:27017/lett_database_test'
+  },
+  consign: [
+    {
+      cwd:'server',
+      verbose:false
+    }
+  ]
 };
